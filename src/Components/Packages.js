41bc9bc1b99d805f530c70658544ba039c221fc8
Ryan Kubik <rmkubik@me.com>
2018-09-21 20:26:39 -0700

    Add more bundles

--- src/Components/Packages.js
+++ src/Components/Packages.js
@@ -16,16 +16,42 @@ export default class Packages extends Component {
               <img src="https://www.outsideonline.com/sites/default/files/styles/img_1400x800/public/2016/07/18/skull-stake-main_h.jpg?itok=Kez5UUEA"/>
               <h3>Welcome to the Jungle...</h3>
             </div>
+
+            <div className={"col"}>
+              <img src="https://c8.alamy.com/comp/BPM5NA/deserted-treasure-island-with-empty-treasure-chest-laying-on-the-shore-BPM5NA.jpg"/>
+              <h3>X Marks the Spot...</h3>
+            </div>
           </div>
           <div className={"row"}>
             <div className={"col"}>
-              <img src="http://i.imgur.com/AqmgJsw.png"/>
+              <img src="https://media.istockphoto.com/photos/pristine-tioman-island-beach-in-malaysia-picture-id696104932"/>
               <h3>On the Beach ...</h3>
             </div>
 
             <div className={"col"}>
-              <img src="https://c8.alamy.com/comp/BPM5NA/deserted-treasure-island-with-empty-treasure-chest-laying-on-the-shore-BPM5NA.jpg"/>
+              <img src="http://i.imgur.com/AqmgJsw.png"/>
               <h3>Cliff Hanger...</h3>
+            </div>
+
+            <div className={"col"}>
+              <img src="https://cdn.cnn.com/cnnnext/dam/assets/180128025828-kiribati-ferry-rescue-01-exlarge-169.jpg"/>
+              <h3>Die Another Day...</h3>
+            </div>
+          </div>
+          <div className={"row"}>
+            <div className={"col"}>
+              <img src="https://www.greenpeace.org/usa/wp-content/uploads/2016/05/GP0STPU67_Web_size_with_credit_line-1.jpg"/>
+              <h3>Exxon Adventure ...</h3>
+            </div>
+
+            <div className={"col"}>
+              <img src="https://media.istockphoto.com/photos/cannibal-chick-picture-id173678965"/>
+              <h3>Dinner with the Donners...</h3>
+            </div>
+
+            <div className={"col"}>
+              <img src="https://i.kym-cdn.com/photos/images/original/000/084/761/whtevan.jpg"/>
+              <h3>Kidnap...</h3>
             </div>
           </div>
         </section>
