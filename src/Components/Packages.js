211b6160cd234f721df42fe78913ae0a276f0b1f
Ryan Kubik <rmkubik@me.com>
2018-09-21 19:51:36 -0700

    Add placeholder images

--- src/Components/Packages.js
+++ src/Components/Packages.js
@@ -7,10 +7,22 @@ export default class Packages extends Component {
         <section>
           <h2>Premade Vacation Bundles</h2>
           <ul>
-            <li>Sink or Swim</li>
-            <li>Welcome to the Jungle</li>
-            <li>On the Beach</li>
-            <li>Cliff Hanger</li>
+            <li>
+              <img src="https://via.placeholder.com/350x250"/>
+              <h3>Sink or Swim...</h3>
+            </li>
+            <li>
+              <img src="https://via.placeholder.com/350x250"/>
+              <h3>Welcome to the Jungle...</h3>
+            </li>
+            <li>
+              <img src="https://via.placeholder.com/350x250"/>
+              <h3>On the Beach...</h3>
+            </li>
+            <li>
+              <img src="https://via.placeholder.com/350x250"/>
+              <h3>Cliff Hanger...</h3>
+            </li>
           </ul>
         </section>
       );
