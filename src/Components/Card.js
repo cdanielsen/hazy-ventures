2b8932f47f302ad5b65d91358c77db84a4f02206
Ryan Kubik <rmkubik@me.com>
2018-09-21 20:18:05 -0700

    Add images

---
+++ src/Components/Card.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import styles from './styles.css';
+
+export default class Packages extends Component {
+    render() {
+      return (
+          <div
+        <div className={"col"}>
+            <img src="https://via.placeholder.com/350x250"/>
+            <h3>Sink or Swim...</h3>
+        </div>
+      );
+    }
+}
--- src/Components/Packages.js
+++ src/Components/Packages.js
@@ -5,26 +5,26 @@ export default class Packages extends Component {
     render() {
       return (
         <section id="packages" className={"container"}>
-          <h2>Premade Vacation Bundles</h2>
+          <h2 className={"display-1"}>Premade Vacation Bundles</h2>
           <div className={"row"}>
             <div className={"col"}>
-              <img src="https://via.placeholder.com/350x250"/>
+              <img src="https://media-cdn.tripadvisor.com/media/photo-s/07/d5/68/86/spider-house-resort.jpg"/>
               <h3>Sink or Swim...</h3>
             </div>
 
             <div className={"col"}>
-              <img src="https://via.placeholder.com/350x250"/>
+              <img src="https://www.outsideonline.com/sites/default/files/styles/img_1400x800/public/2016/07/18/skull-stake-main_h.jpg?itok=Kez5UUEA"/>
               <h3>Welcome to the Jungle...</h3>
             </div>
           </div>
           <div className={"row"}>
             <div className={"col"}>
-              <img src="https://via.placeholder.com/350x250"/>
+              <img src="http://i.imgur.com/AqmgJsw.png"/>
               <h3>On the Beach ...</h3>
             </div>
 
             <div className={"col"}>
-              <img src="https://via.placeholder.com/350x250"/>
+              <img src="https://c8.alamy.com/comp/BPM5NA/deserted-treasure-island-with-empty-treasure-chest-laying-on-the-shore-BPM5NA.jpg"/>
               <h3>Cliff Hanger...</h3>
             </div>
           </div>
