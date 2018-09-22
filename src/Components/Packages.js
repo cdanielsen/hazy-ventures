942624e16050da424e8c42bb14491ccd5f057f44
Ryan Kubik <rmkubik@me.com>
2018-09-21 19:44:51 -0700

    Add basic packages placeholder

--- src/Components/Packages.js
+++ src/Components/Packages.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+// import styles from './styles.css';
 
 export default class Packages extends Component {
     render() {
       return (
-        <div>Packages Goes Here
-        </div>
+        <section>
+          <h2>Premade Vacation Bundles</h2>
+          <ul>
+            <li>Sink or Swim</li>
+            <li>Welcome to the Jungle</li>
+            <li>On the Beach</li>
+            <li>Cliff Hanger</li>
+          </ul>
+        </section>
       );
     }
 }
