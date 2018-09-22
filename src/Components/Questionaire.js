4de7e60b55bb84928973e12fe3b3fe53905f5052
Nick Bernard <nbernard@marketo.com>
2018-09-21 19:53:22 -0700

    Add Bootstrap

--- src/Components/Questionaire.js
+++ src/Components/Questionaire.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import {
+  Button,
+  Container,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Row,
+} from 'reactstrap';
 
 export default class Questionare extends Component {
     render() {
       return (
-        <div>Questionaire Goes Here
-        </div>
+        <Container>
+          <Row>
+            <h1>Tell Us About Yourself</h1>
+          </Row>
+          <Row>
+            <Form>
+              <FormGroup>
+                <Label for="name">Name</Label>
+                <Input name="email" id="name" />
+              </FormGroup>
+
+              <Button color="primary">Get Lost!</Button>
+            </Form>
+          </Row>
+        </Container>
       );
     }
 }
