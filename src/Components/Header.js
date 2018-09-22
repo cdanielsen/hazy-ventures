d40b1d0bc7d0b7b0cd1374704f59971522486142
RJ Zaworski <rj@koan.co>
2018-09-21 20:27:24 -0700

    Add logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import smoke from '../images/smoke.jpg';
+import logo from '../images/hazy-skull.png';
 
 function scrollToPackages () {
     window.scrollTo(0, 1000)
@@ -9,7 +10,7 @@ export default class Header extends Component {
     render() {
       return (
         <div id="header-container">
-            <h1 id="splash-text">Hazy Ventures</h1>
+            <h1 id="splash-text"><img src={logo} alt="Hazy Ventures" /></h1>
             <img src={smoke} id="splash" />
             <button id="book-trip" onClick={scrollToPackages}>Book Your Trip</button>
             <button id="feeling-unlucky">I'm Feeling Unlucky</button>
