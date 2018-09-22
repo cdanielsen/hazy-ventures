7e47a0665c201b82608b5bcc34bfcad8361d274a
Nick Bernard <nbernard@marketo.com>
2018-09-21 20:19:39 -0700

    Add form submitt handler

--- src/Components/Questionaire.js
+++ src/Components/Questionaire.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  Alert,
   Button,
   Container,
   Form,
@@ -8,95 +9,109 @@ import {
   Label,
   Input,
   Row,
+  Col,
 } from 'reactstrap';
 
 export default class Questionare extends Component {
-  handleClick = () => {
-    alert('Good luck!');
+  state = { submitted: false }
+
+  handleSubmit = () => {
+    this.setState({ submitted: true });
   }
 
   render() {
+    const { submitted } = this.state;
+
     return (
       <Container>
-        <Row>
-          <h1>Tell Us About Yourself</h1>
-        </Row>
-        <Row>
-          <Form>
-            <FormGroup>
-              <Label for="name">Name</Label>
-              <Input name="name" id="name" />
-            </FormGroup>
+        { !submitted &&
+          <Row>
+            <Col sm={ { size: 6, offset: 3 } }>
+              <h1>Book Your "Trip!"</h1>
 
-            <FormGroup>
-              <Label for="exampleEmail">Email</Label>
-              <Input type="email" name="email" id="exampleEmail" />
-            </FormGroup>
+              <Form>
+                <FormGroup>
+                  <Label for="name">Name</Label>
+                  <Input name="name" id="name" />
+                </FormGroup>
 
-            <FormGroup>
-              <Label for="not">Where would you <strong>NOT</strong> what to travel to?</Label>
-              <Input type="textarea" name="text" id="not" />
-            </FormGroup>
+                <FormGroup>
+                  <Label for="exampleEmail">Email</Label>
+                  <Input type="email" name="email" id="exampleEmail" />
+                </FormGroup>
 
-            <FormGroup>
-              <Label for="worst">What are your worst vacation experiences?</Label>
-              <Input type="textarea" name="text" id="worst" />
-            </FormGroup>
+                <FormGroup>
+                  <Label for="not">Where would you <strong>NOT</strong> what to travel to?</Label>
+                  <Input type="textarea" name="text" id="not" />
+                </FormGroup>
 
-            <FormGroup>
-              <Label for="vaccinations">Are your vaccinations up to date?</Label>
-              <Input type="select" name="select" id="vaccinations">
-                <option></option>
-                <option>Yes</option>
-                <option>No</option>
-                <option>What vaccinations?</option>
-              </Input>
-            </FormGroup>
+                <FormGroup>
+                  <Label for="worst">What are your worst vacation experiences?</Label>
+                  <Input type="textarea" name="text" id="worst" />
+                </FormGroup>
 
-            <FormGroup>
-              <Label for="adequate">Are they adequate?</Label>
-              <Input type="select" name="select" id="adequate">
-                <option></option>
-                <option>No</option>
-              </Input>
-            </FormGroup>
+                <FormGroup>
+                  <Label for="vaccinations">Are your vaccinations up to date?</Label>
+                  <Input type="select" name="select" id="vaccinations">
+                    <option></option>
+                    <option>Yes</option>
+                    <option>No</option>
+                    <option>What vaccinations?</option>
+                  </Input>
+                </FormGroup>
 
-            <FormGroup>
-              <Label for="alone">Are you travelling alone?</Label>
-              <Input type="select" name="select" id="alone">
-                <option></option>
-                <option>Yes</option>
-                <option>No</option>
-              </Input>
-            </FormGroup>
+                <FormGroup>
+                  <Label for="adequate">Are they adequate?</Label>
+                  <Input type="select" name="select" id="adequate">
+                    <option></option>
+                    <option>No</option>
+                  </Input>
+                </FormGroup>
 
-            <FormGroup>
-              <Label for="blood">Blood Type <span className="text-danger">*required</span></Label>
-              <Input type="select" name="select" id="blood">
-                <option></option>
-                <option>O+</option>
-                <option>O-</option>
-                <option>A+</option>
-                <option>A-</option>
-                <option>B+</option>
-                <option>B-</option>
-                <option>AB+</option>
-                <option>AB-</option>
-              </Input>
-            </FormGroup>
-            <FormGroup>
-              <Label for="card">Credit Card Number</Label>
-              <Input name="card" id="name" />
-            </FormGroup>
+                <FormGroup>
+                  <Label for="alone">Are you travelling alone?</Label>
+                  <Input type="select" name="select" id="alone">
+                    <option></option>
+                    <option>Yes</option>
+                    <option>No</option>
+                  </Input>
+                </FormGroup>
 
-            <FormGroup>
-              <Label for="kin">Next of Kin</Label>
-              <Input name="kin" id="name" />
-            </FormGroup>
+                <FormGroup>
+                  <Label for="blood">Blood Type <span className="text-danger">*required</span></Label>
+                  <Input type="select" name="select" id="blood">
+                    <option></option>
+                    <option>O+</option>
+                    <option>O-</option>
+                    <option>A+</option>
+                    <option>A-</option>
+                    <option>B+</option>
+                    <option>B-</option>
+                    <option>AB+</option>
+                    <option>AB-</option>
+                  </Input>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="card">Credit Card Number</Label>
+                  <Input name="card" id="name" />
+                </FormGroup>
 
-            <Button className="primary" onClick={ this.handleClick }>Get Lost!</Button>
-          </Form>
-        </Row>
+                <FormGroup>
+                  <Label for="kin">Next of Kin</Label>
+                  <Input name="kin" id="name" />
+                </FormGroup>
+
+                <Button className="primary" onClick={ this.handleSubmit }>Get Lost!</Button>
+              </Form>
+            </Col>
+          </Row>
+        }
+
+        {submitted &&
+          <Alert color="success">
+            Thanks! Your information has been posted to Reddit.
+          </Alert>
+        }
       </Container>
     );
   }
