58eb641a873449c9a5a12686f27b67ccb72c591d
Christian Danielsen <christian.danielsen@insitu.com>
2018-09-21 19:55:21 -0700

    add splash

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import smoke from '../images/smoke.jpg';
 
 export default class Header extends Component {
     render() {
       return (
-        <div>
-            Header Goes Here
+        <div id="header-container">
+            <h1 id="splash-text">Hazy Ventures</h1>
+            <img src={smoke} id="splash" />
         </div>
       );
     }
